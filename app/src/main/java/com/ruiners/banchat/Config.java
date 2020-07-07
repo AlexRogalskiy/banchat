6890aed8c9e2a5e8d23865052e96a144cbf06cd4
dvur0g <hoof7@mail.ru>
2020-07-07 21:13:39 +0300

    Исправил баг с двумя приложениями

--- app/src/main/java/com/ruiners/banchat/Config.java
+++ app/src/main/java/com/ruiners/banchat/Config.java
@@ -1,7 +1,7 @@
 package com.ruiners.banchat;
 
 public class Config {
-   public static final String SERVER_URL = "http://76657c8d7630.ngrok.io/";
+   public static final String SERVER_URL = "http://ac3138616cfb.ngrok.io/";
 
    public static final long DEFAULT_ROOM = 0L;
 }
